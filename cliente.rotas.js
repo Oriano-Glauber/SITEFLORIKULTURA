7fa7c5c6dc52116ba021d4c51b2694b4405e1478
MarcoASW <65910584+MarcoASW@users.noreply.github.com>
2020-05-27 20:08:15 -0300

    Update cliente.rotas.js

--- cliente.rotas.js
+++ cliente.rotas.js
@@ -5,27 +5,30 @@ var router = express.Router()
 router.get('/listarclientes', function(req, res){
     res.json('lista de clientes')
     res.json('Lucas Vilar: Estada das Olarias 550, 8263-160, 41992431403, PR, lukviolar.lv@gmail.com')
-    res.json('Marco Antônio Storrer Weber: R. Jacob Valenga 2771, 83305-005,41988778962, PR, marcoweber0405@gmail.com')
 })
 
 //localhost:3000/api/listarnome
 router.get('/listarnome/:nome', function(req, res){
     res.json('lista de clientes pelo nome')
+    res.json('Estada das Olarias 550, 8263-160, 41992431403, PR, lukviolar.lv@gmail.com')
 })
 
 //localhost:3000/api/cliente/listartelefone
 router.get('/listarportelefone/:telefone', function(req, res){
     res.json('lista de cliente com esse telefone')
+    res.json('Lucas Vilar, Estada das Olarias 550, 8263-160, lukviolar.lv@gmail.com')
 })
 
 //localhost:3000/api/cliente/listarporcpftes 
 router.get('/listarporcpf/:CPF', function(req, res){
     res.json('lista dos clientes com esse cpf')
+    res.json('lucas Vilar, Estada das Olarias 550, 8263-160, 41992431403, PR, lukviolar.lv@gmail.com')
 })
 
 //localhost:3000/api/cliente/listaporCEP
 router.get('/listarporCEP/: CEP', function(req, res){
     res.json('lista de clientes com esse CEP')
+    res.json('lucas Vilar, Estada das Olarias 550, 41992431403, PR, lukviolar.lv@gmail.com')
 })
 
 module.exports = router
