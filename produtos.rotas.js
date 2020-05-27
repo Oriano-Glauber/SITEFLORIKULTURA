ba4ee09dabb4fe7f260523a9b7a85496024a24ae
MarcoASW <65910584+MarcoASW@users.noreply.github.com>
2020-05-27 15:53:27 -0300

    Update produtos.rotas.js

--- produtos.rotas.js
+++ produtos.rotas.js
@@ -1,24 +1,40 @@
 const express = require('express')
 var router = express.Router()
 
-//localhost:3000/api/cliente/listarclientes
-router.get('/listarclientes', function(req, res){
-    res.json('lista de clientes')
+//localhost:3000/api/listar/produto
+router.get('/listarprodutos', function(req, res){
+    res.json('lista de produtos')
 })
 
-//localhost:3000/api/listarpornome/lucas
-router.get('/listarpornome/:nome', function(req, res){
-    res.json('lista de clientes com este nome:')
+//localhost:3000/api/produto/listarportipo
+router.get('/listarportipo/:tipo', function(req, res){
+    res.json('lista de tipo de produtos')
 })
 
-//localhost:3000/api/cliente/listarportelefone/41992431403
-router.get('/listarportelefone/:telefone', function(req, res){
-    res.json('lista de clientes com este telefone:')
+//localhost:3000/api/produto/listarporcodigo
+router.get('/listarporcodigo/:codigo', function(req, res){
+    res.json('lista de produtos com esse código')
 })
 
-//localhost:3000/api/cliente/listarporcpf/13283344906
-router.get('/listarporcpf/:cpf', function(req, res){
-    res.json('lista de clientes com este CPF:')
+//localhost:3000/api/produto/listarfornecedor
+router.get('/listarfornecedor/:fornecedor', function(req, res){
+    res.json('lista de fornecedores do produto')
 })
+
+//localhost:3000/api/produto/inserirnovoproduto
+router.post('/inserirnovoproduto', function(req, res){
+    res.json('novo produto inserido')
+})
+
+//localhost:3000/api/produto/listarporpreco
+router.get('/listarporpreco/:preco', function(req, res){
+    res.json('lista de produtos com esse preço')
+})
+
+//localhost:3000/api/produto/inserirpreco
+router.post('/inserirpreco', function(req, res){
+    res.json('novo preço inserido')
+})
+
 
 module.exports = router
