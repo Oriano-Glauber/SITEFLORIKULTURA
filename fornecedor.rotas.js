236cd46f327fe2303bc27f480831e585f49c7d25
MarcoASW <65910584+MarcoASW@users.noreply.github.com>
2020-05-27 20:10:57 -0300

    Update fornecedor.rotas.js

--- fornecedor.rotas.js
+++ fornecedor.rotas.js
@@ -4,32 +4,23 @@ var router = express.Router()
 //localhost:3000/api/fornecedor/listarfornecedor
 router.get('/listarfornecedor', function(req, res){
     res.json('Oriano Glauber Pai, orianoglauber@gmail.com, 876.495.999-03')
-    res.json('Oriano Glauber Junior, orianojunior@gmail.com, 596.485.365-04')
-    res.json('Oriano Glauber Neto, orianoneto@gmail.com, 457.958.125-05')
 })
 
 //localhost:3000/api/fornecedor/listarnomefornec
 router.get('/listarpornomefornec/:nomefornec', function(req, res){
-    res.json('Oriano Glauber')
-    res.json('Oriano Glauber Junior')
-    res.json('Oriano Glauber Neto')
+    res.json('orianoglauber@gmail.com, 876.495.999-03')
 })
 
 //localhost:3000/api/fornecedor/listarporemail
 router.get('/listarpoemail', function(req, res){
     res.json('lista de fornecedores com este email:')
-    res.json('orianoglauber@gmail.com')
-    res.json('orianojunior@gmail.com')
-    res.json('orianoneto@gmail.com')
+    res.json('Oriano Glauber Pai, 876.495.999-03')
 })
 
 //localhost:3000/api/fornecedor/listarCPF
 router.get('/listarcpffornec/: CPF', function(req, res){
     res.json('lista de fornecedores com esse CPF')
-    res.json('876.495.999-03')
-    res.json('596.485.365-04')
-    res.json('457.958.125-05')
-    
+    res.json('Oriano Glauber Pai, orianoglauber@gmail.com')
 })
 
 module.exports = router
