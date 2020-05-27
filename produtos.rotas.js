fb6fb617c1887d0281ddc664fc48e9a532d12da0
MarcoASW <65910584+MarcoASW@users.noreply.github.com>
2020-05-27 20:18:28 -0300

    Update produtos.rotas.js

--- produtos.rotas.js
+++ produtos.rotas.js
@@ -10,37 +10,37 @@ router.get('/listarprodutos', function(req, res){
 //localhost:3000/api/produto/listarportipo
 router.get('/listarportipo/:tipo', function(req, res){
     res.json('lista de tipo de produtos')
-    res.json('planta')
+    res.json('orquídia, 01, esaflores, novo, 70,00, 80,00')
 })
 
 //localhost:3000/api/produto/listarporcodigo
 router.get('/listarporcodigo/:codigo', function(req, res){
     res.json('lista de produtos com esse código')
-    res.json('01')
+    res.json('orquídia, planta, esaflores, novo, 70,00, 80,00')
 })
 
 //localhost:3000/api/produto/listarfornecedor
 router.get('/listarfornecedor/:fornecedor', function(req, res){
     res.json('lista de fornecedores do produto')
-    res.json('esalflores')
+    res.json('orquídia, planta, 01, novo, 70,00, 80,00')
 })
 
 //localhost:3000/api/produto/listarnovosprodutos
 router.get('/listarnovosprodutos/:novo_prodto', function(req, res){
     res.json('listar novos produtos')
-    res.json('novo')
+    res.json('orquídia, planta, 01, esaflores, 70,00 reais, 80,00 reais)
 })
 
 //localhost:3000/api/produto/listarporpreco
 router.get('/listarporpreco/:preco', function(req, res){
     res.json('lista de produtos com esse preço')
-    res.json('70,00')
+    res.json('orquídia, planta, 01, esaflores, novo, 80,00 reais')
 })
 
 //localhost:3000/api/produto/listarnovospreços
 router.get('/listarnovospreços/: novo_preço', function(req, res){
     res.json('lista de novos preços')
-    res.json('80,00 reais')
+    res.json('orquídia, planta, 01, esaflores, novo, 70,00 reais')
 })
 
 
