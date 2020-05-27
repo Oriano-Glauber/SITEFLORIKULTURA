5a9914ff5f628b94168f9633b05708b7adade23b
MarcoASW <65910584+MarcoASW@users.noreply.github.com>
2020-05-27 19:05:47 -0300

    Update fornecedor.rotas.js

--- fornecedor.rotas.js
+++ fornecedor.rotas.js
@@ -1,14 +1,17 @@
 const express = require('express')
 var router = express.Router()
+<script type = "text/javascript">
+var nome = prompt('Digite seu nome aqui');
+</script>
 
-
+if(nome == oriano){
 //localhost:3000/api/fornecedor/listarfornecedor
 router.get('/listarfornecedor', function(req, res){
     res.json('Oriano Glauber Pai, orianoglauber@gmail.com')
     res.json('Oriano Glauber Junior, orianojunior@gmail.com')
     res.json('Oriano Glauber Neto, orianoneto@gmail.com')
 })
-
+}
 //localhost:3000/api/fornecedor/listarnomefornec
 router.get('/listarpornomefornec/:nomefornec', function(req, res){
     res.json('Oriano Glauber')
