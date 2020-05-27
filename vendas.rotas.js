1b9eae1b20bc022a11fb2f220605b827e69b4e0c
MarcoASW <65910584+MarcoASW@users.noreply.github.com>
2020-05-27 19:49:49 -0300

    Update vendas.rotas.js

--- vendas.rotas.js
+++ vendas.rotas.js
@@ -4,21 +4,25 @@ var router = express.Router()
 //localhost:3000/api/listar/vendas
 router.get('/listarvendas', function(req, res){
     res.json('lista de vendas')
+    res.json('copo de leite, 04/05/2004, 673.876.999-07')
 })
 
 //localhost:3000/api/vendas/listadeprodutosvendidos
 router.get('/listarprodutosvendidos', function(req, res){
     res.json('lista de produtos vendidos desse modelo')
+    res.json('copo de leite')
 })
 
 //localhost:3000/api/vendas/listapordatadevenda
 router.get('/listarpordatadevenda/:data_de_venda', function(req, res){
     res.json('lista de pordutos vendidos nessa data')
+    res.json('04/05/2004')
 })
 
 //localhost:3000/api/vendas/listaporcpfcomprador
 router.get('/listarporcpfcomprador/:cpf_comprador', function(req, res){
     res.json('lista de produtos comprados com esse CPF')
+    res.json('673.876.999.07')
 })
 
 
