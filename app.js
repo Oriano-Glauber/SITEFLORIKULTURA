41719500c56161640d64641e577e27fd8991451e
lucasdxl <65911026+lucasdxl@users.noreply.github.com>
2020-05-27 15:43:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.use(express.static('css'))
 
 app.use('/api/cliente', require('./rotas/cliente.rotas'))
 app.use('/api/fornecedor', require('./rotas/fornecedor.rotas'))
+app.use('/api/estoque', require('./rotas/estoque.rotas'))
+app.use('/api/produto', require('./rotas/produto.rotas'))
+app.use('/api/vendas', require('.rotas/vendas.rotas'))
 
 
 let server = app.listen(3000, function(){
